38ce02217f64f9ea079153ca372f6e26b06410b2
JackQinPSU <qxq5086@psu.edu>
2025-07-23 16:53:20 -0400

    Added email feature to send in messages through the contact me page, ensure sensitive info is protected.

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/submit', (req, res) => {
-    const{name, email, message} = req.body;
-    res.redirect('/thankyou.html');
+// Nodemailer transporter
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
 });
 
+//POST route for form submission
+app.post('/submit', (req, res) => {
+    const{name, email, message} = req.body;
+
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_USER,
+        subject: `New Contact Form Submission from ${name}`,
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Email failed:', error)
+            return res.status(500).send('Something went wrong, please try again later.');
+        }
+        console.log('Email send', info.response);
+        res.redirect('/thankyou.html');
+    });    
+});
+
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
